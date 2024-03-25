76c2f7777b6e671e3df276c9e40993108339fe20
BorisDeLaMar <shumeyko.ilya@gmail.com>
2024-03-25 21:48:30 +0300

    3.5.4

--- 3. Процессы и потоки/3.5 Задачи на программирование/4/solution.c
+++ 3. Процессы и потоки/3.5 Задачи на программирование/4/solution.c
@@ -4,43 +4,30 @@
 #include <stdio.h>  
 #include <stdlib.h>
 
-int recursive_search(int pid)
+int filter(struct dirent **namelist, int ppid, int n)
 {
-	char *path = malloc(1000*sizeof(char)); FILE* file = NULL; int ppid;
-	sprintf(path, "/proc/%d/stat", pid);
-	file = fopen(path, "r");
-	if(file != NULL)
+	int i; int ans = 0;
+	for(i = 0; i < n; i++)
 	{
-		fscanf(file, "%*d %*s %*c %d", &ppid);
-		free(path);
-		fclose(file);
-	} else{
-		printf("Can't open file %s or read ppid field\n", path);
-		free(path);
-		return 0;
-	}
-	return ppid;
-}
-
-int filter(const char *name, int ppid)
-{
-	char *path = malloc(1000*sizeof(char));  
-	int ppid_read; int ans = 0; int pid;
-	sprintf(path, "%s%s%s", "/proc/", name, "/stat");
-	FILE* f = NULL;
-	if((f = fopen(path, "r")) == NULL)
-	{
-		free(path);
-	} else
-	{
-		fscanf(f, "%d", &pid);
-		fscanf(f, "%*c %d", &ppid_read);
-		if(ppid_read == ppid){
-			ans += filter(itoa(pid), );
+		char *path = malloc(1000*sizeof(char));  
+		int ppid_read; int pid;
+		sprintf(path, "%s%s%s", "/proc/", namelist[i]->d_name, "/stat");
+		FILE* f = NULL;
+		if((f = fopen(path, "r")) == NULL)
+		{
 			free(path);
-			fclose(f);
+		} else
+		{
+			fscanf(f, "%d", &pid);
+			fscanf(f, "%*s %*c %d", &ppid_read);
+			if(ppid_read == ppid){
+				ans += 1;
+				ans += filter(namelist, pid, n);
+				free(path);
+				fclose(f);
+			}
 		}
-	}	
+	}		
 	return ans;
 }
 
@@ -51,9 +38,9 @@ int main(int argc, char *argv[])
 	int ans = 1;
 	long n = scandir("/proc/", &namelist, NULL, alphasort);
 	size_t i;
-	for (i = 0; i < n; i++)
+	ans += filter(namelist, ppid, n);
+	for(i = 0; i < n; i++)
 	{
-		ans += filter(namelist[i]->d_name, ppid);
 		free(namelist[i]);
 	}
 	free(namelist);
